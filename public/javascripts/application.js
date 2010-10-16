f356a188ee21d1af02efea67ef3d3b3ae2ed501d
Ryan Bates <ryan@railscasts.com>
2010-10-15 23:11:51 -0700

    adding javascript code to walk through moves

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,35 @@
+var moves = "";
+var current_move = 0;
 $(function() {
-  $("#board .e").live("click", function() {
-    $(this).removeClass("e").addClass("b");
-    $.post(window.location.pathname + '/moves', {"move": $(this).attr("id")}, null, "script");
-    // Show updating progress here
-  });
+  if ($("#board").length > 0) {
+    moves = $("#board").attr("data-moves").split("-");
+    current_move = moves.length;
+    $("#board .e").live("click", function() {
+      // $(this).removeClass("e").addClass("b");
+      $.post(window.location.pathname + '/moves', {"move": $(this).attr("id"), "after": moves.length}, null, "script");
+      // Show updating graphic here
+    });
+  }
 });
 
+function addMoves(new_moves) {
+  $.each(new_moves.split("-"), function(index, move) {
+    moves.push(move);
+    if (current_move == moves.length-1) {
+      stepMove(1, false);
+    }
+  });
+}
+
+function stepMove(step, reverse) {
+  var move_index = current_move-1 + step;
+  var color = (move_index % 2 ? "w" : "b");
+  $.each(moves[move_index].match(/../g), function(index, position) {
+    if (index == 0) {
+      $("#" + position).attr("class", (reverse ? "e" : color));
+    } else {
+      $("#" + position).attr("class", (reverse ? color : "e"));
+    }
+  });
+  current_move += step;
+}
