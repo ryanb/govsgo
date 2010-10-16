df39eee06e1bbb8284cc335f263fa1f322cf6e6d
Ryan Bates <ryan@railscasts.com>
2010-10-16 01:24:58 -0700

    adding prevoius/next links under game board for scrolling through positions

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -10,6 +10,18 @@ $(function() {
       $.post(window.location.pathname + '/moves', {"move": $(this).attr("id"), "after": moves.length}, null, "script");
       // Show updating graphic here
     });
+    $("#previous_move").click(function() {
+      if (current_move > 0) {
+        stepMove(-1);
+      }
+      return false;
+    });
+    $("#next_move").click(function() {
+      if (current_move < moves.length) {
+        stepMove(1);
+      }
+      return false;
+    });
   }
 });
 
@@ -17,19 +29,19 @@ function addMoves(new_moves) {
   $.each(new_moves.split("-"), function(index, move) {
     moves.push(move);
     if (current_move == moves.length-1) {
-      stepMove(1, false);
+      stepMove(1);
     }
   });
 }
 
-function stepMove(step, reverse) {
+function stepMove(step) {
   current_move += step;
   var color = (current_move % 2 ? "b" : "w");
-  $.each(moves[current_move-1].match(/../g), function(index, position) {
+  $.each(moves[step > 0 ? current_move-1 : current_move].match(/../g), function(index, position) {
     if (index == 0) {
-      $("#" + position).attr("class", (reverse ? "e" : color));
+      $("#" + position).attr("class", (step > 0 ? color : "e"));
     } else {
-      $("#" + position).attr("class", (reverse ? color : "e"));
+      $("#" + position).attr("class", (step > 0 ? "e" : color));
     }
   });
 }
