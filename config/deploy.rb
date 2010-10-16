47695e85159aaa8b66010bb0fab039243ee7b6d9
Ryan Bates <ryan@railscasts.com>
2010-10-15 20:40:40 -0700

    updating deploy.rb script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,26 +1,4 @@
-set :application, "govsgo.com"
-set :repository,  "set your repository location here"
-
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
-
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
-
+require "bundler/capistrano"
 
 set :application, "govsgo.com"
 role :app, application
@@ -88,4 +66,3 @@ after "deploy", "deploy:cleanup" # keeps only last 5 releases
 after "deploy:setup", "deploy:setup_shared"
 after "deploy:update_code", "deploy:symlink_extras"
 # after "deploy:symlink", "deploy:update_crontab"
-
