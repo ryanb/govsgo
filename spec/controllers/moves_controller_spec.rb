912fcb0467f80903ec98fff65a1272f760811ae4
James Edward Gray II <james@graysoftinc.com>
2010-10-15 22:09:12 -0500

    Passing needed params.

--- spec/controllers/moves_controller_spec.rb
+++ spec/controllers/moves_controller_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe MovesController do
   it "should add a move and respond with javascript" do
-    game = Game.create!
-    post "create", :game_id => game.id, :format => "js"
+    game = Game.create!(board_size: 19)
+    post "create", :game_id => game.id, :format => "js", :move => "aa"
     response.should be_success
   end
 end
