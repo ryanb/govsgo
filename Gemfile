5e26ddbced850e21a07f4313a3505a783c45b9d8
Ryan Bates <ryan@railscasts.com>
2010-10-15 18:32:41 -0700

    generating some simple user authentication

--- Gemfile
+++ Gemfile
@@ -4,9 +4,10 @@ gem 'rails', '3.0.0'
 gem 'mysql2'
 gem 'nifty-generators'
 gem 'jquery-rails'
+gem 'bcrypt-ruby', :require => 'bcrypt'
 
 group :development, :test do
-  gem "mocha"
+  gem 'mocha'
   gem 'rspec-rails', '>= 2.0.1'
   gem 'factory_girl_rails'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Govsgo::Application.routes.draw do
+  match 'user/edit' => 'users#edit', :as => :edit_current_user
+  match 'signup' => 'users#new', :as => :signup
+  match 'logout' => 'sessions#destroy', :as => :logout
+  match 'login' => 'sessions#new', :as => :login
+  resources :sessions
+  resources :users
   resources :games do
     resources :moves
   end
