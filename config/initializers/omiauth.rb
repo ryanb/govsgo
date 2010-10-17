cf2ac31502cc0a141bc9beca26975b1e41ce1b9b
Ryan Bates <ryan@railscasts.com>
2010-10-16 17:03:38 -0700

    adding facebook id and secret for omniauth

--- config/initializers/omiauth.rb
+++ config/initializers/omiauth.rb
@@ -1,5 +1,6 @@
 require 'openid/store/filesystem'
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, PRIVATE_CONFIG["twitter_key"], PRIVATE_CONFIG["twitter_secret"]
+  provider :facebook, PRIVATE_CONFIG["facebook_id"], PRIVATE_CONFIG["facebook_secret"]
   provider :open_id, OpenID::Store::Filesystem.new('/tmp')
 end
