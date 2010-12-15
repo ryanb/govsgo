f2533b8a831521dc565178e8bc9cc9177f1707a7
Ryan Bates <ryan@railscasts.com>
2010-12-14 16:15:44 -0800

    hide the game panel when navigating moves

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -62,28 +62,28 @@ function setupGame() {
     return false;
   });
   $("#previous_move").click(function() {
-    $("#message_overlay").hide();
+    $("#panel_container").html("");
     if (current_move > 0) {
       stepMove(-1, false);
     }
     return false;
   });
   $("#next_move").click(function() {
-    $("#message_overlay").hide();
+    $("#panel_container").html("");
     if (current_move < moves.length) {
       stepMove(1, false);
     }
     return false;
   });
   $("#first_move").click(function() {
-    $("#message_overlay").hide();
+    $("#panel_container").html("");
     while (current_move > 0) {
       stepMove(-1, true);
     }
     return false;
   });
   $("#last_move").click(function() {
-    $("#message_overlay").hide();
+    $("#panel_container").html("");
     while (current_move < moves.length) {
       stepMove(1, true);
     }
