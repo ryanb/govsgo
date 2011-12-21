719f34fed77cf96b728940d2d3b18d0a8ee1b29b
quake wang <quake.wang@gmail.com>
2011-12-21 11:54:44 +0800

    add i18n related files (WIP)

---
+++ lib/controller_i18n.rb
@@ -0,0 +1,32 @@
+module ControllerI18n
+  def self.included(controller)
+    controller.send :before_filter, :set_locale
+  end
+  
+  private
+  def set_locale
+    I18n.locale = cookies[:locale] || setup_cookies_locale
+  end
+
+  def change_locale
+    cookies[:locale] = current_user.locale unless current_user.locale.blank?
+  end
+
+  def setup_cookies_locale
+    if logged_in?
+      current_user.update_attribute(:locale, extract_locale_from_accept_language_header) if current_user.locale.blank?
+      cookies[:locale] = current_user.locale
+    else
+      cookies[:locale] = extract_locale_from_accept_language_header
+    end
+  end
+
+  def extract_locale_from_accept_language_header
+    if lang = request.accept_language
+      locale = lang.split(",").first.downcase.to_sym
+    else
+      locale = nil
+    end
+    I18n.available_locales.include?(locale) ? locale : I18n.default_locale
+  end
+end
